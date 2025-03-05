a9dd17e5080e98f8130f5fcf0e03e29d44569c76
Dmitriy Vasilev <raoffonom@icloud.com>
2025-03-05 17:26:28 +0700

    open router

--- src/core/openai/index.ts
+++ src/core/openai/index.ts
@@ -1,15 +1,24 @@
 import OpenAI from 'openai'
 
-if (!process.env.DEEPSEEK_API_KEY) {
-  throw new Error('DEEPSEEK_API_KEY is not set')
+// if (!process.env.DEEPSEEK_API_KEY) {
+//   throw new Error('DEEPSEEK_API_KEY is not set')
+// }
+
+// export const openai = new OpenAI({
+//   baseURL: 'https://api.deepseek.com',
+//   apiKey: process.env.DEEPSEEK_API_KEY,
+// })
+
+if (!process.env.OPENROUTER_API_KEY) {
+  throw new Error('OPENROUTER_API_KEY is not set')
 }
 
 export const openai = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
-  apiKey: process.env.DEEPSEEK_API_KEY,
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.OPENROUTER_API_KEY,
 })
 
-export const model = 'deepseek-chat'
+export const model = 'deepseek/deepseek-r1:free'
 
 export * from './getSubtitles'
 export * from './getTriggerReel'
