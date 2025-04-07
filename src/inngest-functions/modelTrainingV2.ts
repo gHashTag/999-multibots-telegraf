815fe922d9801b8c19659265e42e9a696dfe2bc2
Dmitriy Vasilev <raoffonom@icloud.com>
2025-04-07 21:12:27 +0700

    feat: добавлен импорт fetch из undici

--- src/inngest-functions/modelTrainingV2.ts
+++ src/inngest-functions/modelTrainingV2.ts
@@ -10,6 +10,7 @@ import { ModeEnum, calculateModeCost } from '@/price/helpers/modelsCost'
 import { supabase } from '@/core/supabase'
 import { v4 as uuidv4 } from 'uuid'
 import { logger } from '@/utils/logger'
+import { fetch } from 'undici'
 
 import axios from 'axios'
 
