1a4405935119bc06d9d8e29de0c0b19186eca606
Dmitriy Vasilev <raoffonom@icloud.com>
2025-04-11 00:59:21 +0700

    Удалены устаревшие тесты для перехода на новую систему тестирования

--- src/test-utils/tests/index.ts
+++
@@ -1,5 +0,0 @@
-// Экспортируем все тесты
-import { testImageToPrompt } from './imageToPrompt.test'
-import { testPaymentProcessor } from './paymentProcessor.test'
-
-export { testImageToPrompt, testPaymentProcessor }
