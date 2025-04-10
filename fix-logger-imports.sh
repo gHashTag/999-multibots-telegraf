2f35508e61cc66f34644f3607ef2ba05fb5ce55f
Dmitriy Vasilev <raoffonom@icloud.com>
2025-04-11 06:37:00 +0700

    Fix logger imports and Docker build process

--- fix-logger-imports.sh
+++ fix-logger-imports.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Fix absolute imports
-find src -type f -name "*.ts" -exec sed -i '' 's/import { logger } from '\''@\/utils\/logger'\''/import { Logger as logger } from '\''@\/utils\/logger'\''/g' {} +
+find src -type f -name "*.ts" -exec sed -i '' 's/import { Logger as logger } from '\''@\/utils\/logger'\''/import { logger } from '\''@\/utils\/logger'\''/g' {} +
 
 # Fix relative imports with ../../
-find src -type f -name "*.ts" -exec sed -i '' 's/import { logger } from '\''\.\.\/\.\.\/utils\/logger'\''/import { Logger as logger } from '\''\.\.\/\.\.\/utils\/logger'\''/g' {} +
+find src -type f -name "*.ts" -exec sed -i '' 's/import { Logger as logger } from '\''\.\.\/\.\.\/utils\/logger'\''/import { logger } from '\''\.\.\/\.\.\/utils\/logger'\''/g' {} +
 
 # Fix relative imports with ../
-find src -type f -name "*.ts" -exec sed -i '' 's/import { logger } from '\''\.\.\/utils\/logger'\''/import { Logger as logger } from '\''\.\.\/utils\/logger'\''/g' {} + 
+find src -type f -name "*.ts" -exec sed -i '' 's/import { Logger as logger } from '\''\.\.\/utils\/logger'\''/import { logger } from '\''\.\.\/utils\/logger'\''/g' {} + 
