04b5ca3f619583ab3ace232cc8bd0f0bb9f2d4a3
Dmitriy Vasilev <raoffonom@icloud.com>
2025-04-11 21:32:24 +0700

    clean

--- src/scenes/subscriptionCheckScene.ts
+++ src/scenes/subscriptionCheckScene.ts
@@ -6,7 +6,6 @@ import { getSubScribeChannel } from '@/core/supabase'
 import { isDev } from '@/helpers'
 import { ModeEnum } from '@/price/helpers/modelsCost'
 import { logger } from '@/utils/logger'
-import { SceneEnum } from '@/types/scenes'
 
 const subscriptionCheckStep = async (ctx: MyContext) => {
   if (!ctx.from?.id) {
@@ -57,9 +56,9 @@ const subscriptionCheckStep = async (ctx: MyContext) => {
   }
 
   if (ctx.session.mode === ModeEnum.MainMenu) {
-    return ctx.scene.enter(ModeEnum.MainMenu)
+    return ctx.scene.enter('menuScene')
   } else {
-    return ctx.scene.enter(SceneEnum.MainMenu)
+    return ctx.scene.enter(ctx.session.mode)
   }
 }
 
