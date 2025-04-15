828d9c1520b559449c6b2c662c0477c92055f897
Dmitriy Vasilev <raoffonom@icloud.com>
2025-04-15 23:26:58 +0700

    tests

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -6,3 +6,11 @@ export enum NeuroPromptStatus {
   Completed = 'completed',
   Failed = 'failed',
 }
+
+export enum ModeEnum {
+  TextToImage = 'text_to_image',
+  TextToVideo = 'text_to_video',
+  ImageToImage = 'image_to_image',
+  TopUpBalance = 'top_up_balance',
+  Subscription = 'subscription',
+}
