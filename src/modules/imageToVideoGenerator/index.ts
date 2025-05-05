88025edfe3733bb055d4027c39b584a1e9498f94
Dmitriy Vasilev <raoffonom@icloud.com>
2025-05-05 20:48:12 +0700

    video

--- src/modules/imageToVideoGenerator/index.ts
+++ src/modules/imageToVideoGenerator/index.ts
@@ -1,3 +1,4 @@
 export { generateImageToVideo } from './generateImageToVideo'
+export { generateTextToVideo } from './generateTextToVideo'
 // Re-export VideoModelConfig from its NEW local location
 export { type VideoModelConfig } from './config/models.config'
