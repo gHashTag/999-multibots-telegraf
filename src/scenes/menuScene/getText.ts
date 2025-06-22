468b6acc2b1e345430722701416327109fb79733
Dmitriy Vasilev <raoffonom@icloud.com>
2025-06-22 16:12:07 +0700

    fix

--- src/scenes/menuScene/getText.ts
+++ src/scenes/menuScene/getText.ts
@@ -1,6 +1,6 @@
 const texts = {
   ru: {
-    digitalAvatar: `🚀 Чтобы начать создавать нейрофотографии, вам нужно обучить ИИ модель на ваших фотографиях. Для этого, пожалуйста, оформите подписку на бота, чтобы получить доступ к этой функции!\n\nПредставь, как твои фотографии превращаются в стильные и современные произведения искусства, подчеркивая твою индивидуальность и выделяя из толпы.\n\nЧтобы начать создавать нейрофотографии, Выберите команду 💫 Оформить подписку в главном меню`,
+    digitalAvatar: `🚀 Чтобы начать создавать нейрофотографии, вам нужно обучить ИИ модель на ваших фотографиях. Для этого оформите подписку на бота, чтобы получить доступ к этой функции!\n\nПредставь, как твои фотографии превращаются в стильные и современные произведения искусства, подчеркивая твою индивидуальность и выделяя из толпы.`,
     neurophoto: `🚀 Чтобы начать создавать нейрофотографии, Выберите команду 📸 Нейрофото в главном меню`,
     avatarLevel: (newCount: number) =>
       `🆔 Уровень вашего аватара: ${newCount} \n\n🤖 Чтобы начать пользоваться ботом нажмите команду /menu\n\n🔓 Хотите разблокировать все функции?\n💫 Оформите подписку, чтобы получить полный доступ!`,
@@ -8,7 +8,7 @@ const texts = {
     inviteLink: 'Ссылка для приглашения друзей 👇🏻',
   },
   en: {
-    digitalAvatar: `🚀 To start creating neurophotographs, you need to train the AI model on your photos. Please subscribe to the bot to access this feature!\n\nImagine your photos turning into stylish and modern art pieces, highlighting your individuality and standing out from the crowd.\n\nTo start creating neurophotographs, select the 💫 Subscribe command in the main menu`,
+    digitalAvatar: `🚀 To start creating neurophotographs, you need to train the AI model on your photos. Please subscribe to the bot to access this feature!\n\nImagine your photos turning into stylish and modern art pieces, highlighting your individuality and standing out from the crowd.`,
     neurophoto: `🚀 To start creating neurophotographs, Выберите команду 📸 Нейрофото в главном меню`,
     avatarLevel: (newCount: number) =>
       `🚀 To unlock the next level of the avatar and gain access to new features, invite friend! 🌟\n\n🆔 Level your avatar: ${newCount} invitations \n\n🤖 To start using the bot, click the /menu command\n\n🔓 Want to unlock all features?\n💫 Subscribe to get full access!`,
