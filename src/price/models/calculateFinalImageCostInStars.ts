7a6b915937ad8970e4beb76a6e89925995cbfcff
Dmitriy Vasilev <raoffonom@icloud.com>
2025-10-30 15:13:40 +0700

    checkpoint: Так еще раз проанализирую ценообразование.
    Ка...
    
    💎 Built using [Crystal](https://github.com/stravu/crystal)
    
    Co-Authored-By: Crystal <crystal@stravu.com>

--- src/price/models/calculateFinalImageCostInStars.ts
+++ src/price/models/calculateFinalImageCostInStars.ts
@@ -2,6 +2,7 @@ import { SYSTEM_CONFIG } from '@/price/constants/index'
 
 // Функция для расчета окончательной стоимости изображения в звездах
 export function calculateFinalImageCostInStars(baseCost: number): number {
-  const finalCostInDollars = baseCost * (1 + SYSTEM_CONFIG.interestRate)
+  // FIXED: Use interestRate as multiplier (1.5 = 50% markup), not addition (was 1 + 1.5 = 150% markup)
+  const finalCostInDollars = baseCost * SYSTEM_CONFIG.interestRate
   return Math.ceil(finalCostInDollars / SYSTEM_CONFIG.starCost)
 }
