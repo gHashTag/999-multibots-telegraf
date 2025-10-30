0c3efeaf7897c9ae67d11da59b8b133eb77e17eb
Dmitriy Vasilev <raoffonom@icloud.com>
2025-10-30 15:49:28 +0700

    checkpoint: Давай перенесем все ingest функции и протестир...
    💎 Built using [Crystal](https://github.com/stravu/crystal)
    
    Co-Authored-By: Crystal <crystal@stravu.com>

---
+++ src/inngest_app/functions/index.ts
@@ -0,0 +1,69 @@
+/**
+ * Master Index - All Inngest Functions
+ * Complete migration from ai-server
+ * Fully isolated, no external dependencies
+ */
+
+// Content Functions
+export { analyzeCompetitorReelsFunction } from './content/analyzeCompetitorReels'
+export { extractTopContentFunction } from './content/extractTopContent'
+export { findCompetitorsFunction } from './content/findCompetitors'
+export { generateContentScriptsFunction } from './content/generateContentScripts'
+export { generateDetailedScriptFunction } from './content/generateDetailedScript'
+export { generateScenarioClipsFunction } from './content/generateScenarioClips'
+
+// Instagram Functions
+export { instagramScraperV2Function } from './instagram/instagramScraper-v2'
+export { instagramScraperV2SimpleFunction } from './instagram/instagramScraper-v2-simple'
+
+// Monitoring Functions
+export { criticalErrorMonitorFunction } from './monitoring/criticalErrorMonitor'
+export { logMonitorFunction } from './monitoring/logMonitor'
+
+// Training Functions
+export { modelTrainingV2Function } from './training/modelTrainingV2'
+export { morphImagesFunction } from './training/morphImages'
+
+// Generation Functions
+export { neuroImageGenerationFunction } from './generation/neuroImageGeneration'
+
+// Payment Functions
+export { paymentProcessingFunction } from './payments/paymentProcessing'
+
+// Broadcast Functions
+export { broadcastMessageFunction } from './broadcast/broadcastMessage'
+
+// Render Functions (complete module)
+export * from './render'
+
+// Existing Functions (already in telegraf)
+export { generateAIReelsFunction } from './existing/generateAIReelsFunction'
+export { generateAdvancedLoopingVideoFunction } from './existing/generateAdvancedLoopingVideoFunction'
+export { generateModelTrainingFunction } from './existing/generateModelTrainingFunction'
+
+// Helper functions for all Inngest functions
+export const getAllFunctions = () => [
+  analyzeCompetitorReelsFunction,
+  extractTopContentFunction,
+  findCompetitorsFunction,
+  generateContentScriptsFunction,
+  generateDetailedScriptFunction,
+  generateScenarioClipsFunction,
+  instagramScraperV2Function,
+  instagramScraperV2SimpleFunction,
+  criticalErrorMonitorFunction,
+  logMonitorFunction,
+  modelTrainingV2Function,
+  morphImagesFunction,
+  neuroImageGenerationFunction,
+  paymentProcessingFunction,
+  broadcastMessageFunction,
+  // Render functions
+  renderFunction,
+  renderAvatarVideoFunction,
+  renderRiddleFunction,
+  // Existing functions
+  generateAIReelsFunction,
+  generateAdvancedLoopingVideoFunction,
+  generateModelTrainingFunction
+]
